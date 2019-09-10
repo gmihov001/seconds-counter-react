810ea220c5d7d76775d797512b7e3d9be47dba87
George Mihov <gmihov001@gmail.com>
2019-09-10 19:06:37 +0000

    Initial

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,39 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+//import { Home } from "./component/home.js";
+
+class SecondsCounter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>
+					<i className="fa fa-clock-o" aria-hidden="true" />
+					{("00000" + this.state.count).slice(-6)}
+				</h1>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		this.myInterval = setInterval(() => {
+			this.setState(prevState => ({
+				count: prevState.count + 1
+			}));
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.myInterval);
+	}
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
