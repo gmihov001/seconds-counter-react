6aa84b458103421d8d35fc2559e228506f69a33d
George Mihov <gmihov001@gmail.com>
2019-09-12 19:51:22 +0000

    Final version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,26 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//include bootstrap npm library into the bundle
+import "bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
 
 //create your first component
-export class Home extends React.Component {
-	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
-			</div>
-		);
-	}
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 //import react into the bundle
+//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
@@ -11,37 +15,47 @@ import "../styles/index.scss";
 //import your own components
 //import { Home } from "./component/home.js";
 
-class SecondsCounter extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			count: 0
-		};
-	}
+let seconds = 0;
+var counter;
+setInterval(myTimer, 1000);
 
-	render() {
-		return (
-			<div>
-				<h1>
-					<i className="fa fa-clock-o" aria-hidden="true" />
-					{("00000" + this.state.count).slice(-6)}
-				</h1>
-			</div>
-		);
-	}
-
-	componentDidMount() {
-		this.myInterval = setInterval(() => {
-			this.setState(prevState => ({
-				count: prevState.count + 1
-			}));
-		}, 1000);
-	}
-
-	componentWillUnmount() {
-		clearInterval(this.myInterval);
-	}
+function myTimer() {
+	seconds += 1;
+	counter = ("000000" + seconds).slice(-6);
+	ReactDOM.render(
+		<Home
+			char1={counter.charAt(0)}
+			char2={counter.charAt(1)}
+			char3={counter.charAt(2)}
+			char4={counter.charAt(3)}
+			char5={counter.charAt(4)}
+			char6={counter.charAt(5)}
+		/>,
+		document.querySelector("#app")
+	);
 }
 
-//render your react application
-ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
+function Home(props) {
+	return (
+		<div className="row mx-0 px-5 py-1 d-flex justify-content-center">
+			<div className="col col-1 text-white p-0 mx-2">
+				<i className="far fa-clock" />
+			</div>
+			<div className="col col-1 text-white p-0 mx-2">{props.char1}</div>
+			<div className="col col-1 text-white p-0 mx-2">{props.char2}</div>
+			<div className="col col-1 text-white p-0 mx-2">{props.char3}</div>
+			<div className="col col-1 text-white p-0 mx-2">{props.char4}</div>
+			<div className="col col-1 text-white p-0 mx-2">{props.char5}</div>
+			<div className="col col-1 text-white p-0 mx-2">{props.char6}</div>
+		</div>
+	);
+}
+
+Home.propTypes = {
+	char1: PropTypes.number,
+	char2: PropTypes.number,
+	char3: PropTypes.number,
+	char4: PropTypes.number,
+	char5: PropTypes.number,
+	char6: PropTypes.number
+};
